4f2a8c1220b114da25f64a444c33fba11c20f8c6
sabreenMohammed107 <sabreenm312@gmail.com>
2022-11-02 17:16:47 +0200

    2-11-2022

---
+++ database/migrations/2022_11_02_115638_add_data_to_companies_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            //
+            $table->string('master_page_img_bg', 250)->after('id')->nullable();
+            $table->string('master_page_entitle', 250)->after('master_page_img_bg')->nullable();
+            $table->string('master_page_artitle', 250)->after('master_page_entitle')->nullable();
+            $table->string('master_page_ensubtitle', 250)->after('master_page_artitle')->nullable();
+            $table->string('master_page_arsubtitle', 250)->after('master_page_ensubtitle')->nullable();
+            $table->text('master_page_entext')->after('master_page_arsubtitle')->nullable();
+            $table->text('master_page_artext')->after('master_page_entext')->nullable();
+            $table->string('limit_offer_endesc', 250)->after('master_page_artext')->nullable();
+            $table->string('limit_offer_ardesc', 250)->after('limit_offer_endesc')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ database/migrations/2022_11_02_120926_create_offers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('city_id')->nullable();
+            $table->foreign('city_id')->references('id')->on('cities');
+            $table->string('subtitle_en', 250)->nullable();
+            $table->string('subtitle_ar', 250)->nullable();
+            $table->string('image', 250)->nullable();
+            $table->integer('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+};
