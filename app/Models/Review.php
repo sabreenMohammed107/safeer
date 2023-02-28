52423baa9f76da0a5583de5991aa9e6202eeef16
sabreenMohammed107 <sabreenm312@gmail.com>
2023-03-01 00:20:50 +0200

    test review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'review_text',
+    'review_date',
+    'review_stars',
+    'active',
+    'hotel_id',
+    'tour_id',
+
+
+    ];
+
+    public function user(){
+        return $this->belongsTo(SiteUser::class,'user_id');
+    }
+
+
 }
