99dd649f1b9fe89eca1f4af4cbbb9f635e3415c8
sabreenMohammed107 <sabreenm312@gmail.com>
2023-03-01 15:30:58 +0200

    test whataapp

--- resources/views/layout/website/layout.blade.php
+++ resources/views/layout/website/layout.blade.php
@@ -59,6 +59,7 @@
     {{-- owl Carousel links --}}
     <!-- icon -->
     <link rel="icon" href="{{ asset('/website_assets/images/homePage/logo.webp') }}">
+    <link rel="stylesheet" href="{{ asset('/website_assets/css/whatsappStyle.css') }}">
 
     @yield("adds_css")
     <title> {{$title}} </title>
@@ -316,6 +317,55 @@
     {{-- owl carousel --}}
     <script>
         $(document).ready(function(){
+            // whts pp
+$(document).on("click", "#send-it", function() {
+  var a = document.getElementById("chat-input");
+  if ("" != a.value) {
+    var b = $("#get-number").text(),
+      c = document.getElementById("chat-input").value,
+      d = "https://web.whatsapp.com/send",
+      e = b,
+      f = "&text=" + c;
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    )
+      var d = "whatsapp://send";
+    //   +31 6 29320129
+    var g = d + "?phone=+905445019185" + e + f;
+    window.open(g, "_blank");
+  }
+}),
+  $(document).on("click", ".informasi", function() {
+    (document.getElementById("get-number").innerHTML = $(this)
+      .children(".my-number")
+      .text()),
+      $(".start-chat,.get-new")
+        .addClass("show")
+        .removeClass("hide"),
+      $(".home-chat,.head-home")
+        .addClass("hide")
+        .removeClass("show"),
+      (document.getElementById("get-nama").innerHTML = $(this)
+        .children(".info-chat")
+        .children(".chat-nama")
+        .text()),
+      (document.getElementById("get-label").innerHTML = $(this)
+        .children(".info-chat")
+        .children(".chat-label")
+        .text());
+  }),
+  $(document).on("click", ".close-chat", function() {
+    $("#whatsapp-chat")
+      .addClass("hide")
+      .removeClass("show");
+  }),
+  $(document).on("click", ".blantershow-chat", function() {
+    $("#whatsapp-chat")
+      .addClass("show")
+      .removeClass("hide");
+  });
                 $(".owl-carousel").owlCarousel({
                     items:4,
                     loop:true,
--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -1,9 +1,5 @@
 @extends("layout.website.layout", ["Company" => $Company,"title"=>"Safer | Home"])
-@section('adds_css')
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ asset('/website_assets/css/whatsappStyle.css') }}">
 
-@endsection
 @section("bottom-header")
 <x-website.header.home :company="$Company" :countries="$Countries" :cities="$cities" />
 @endsection
@@ -12,58 +8,7 @@
 
     </style>
 
-<div id='whatsapp-chat' class='hide'>
-    <div class='header-chat'>
-      <div class='head-home'>
-        <div class='info-avatar'>
-            <img src="{{asset("/website_assets/images/llogo.JPG")}}" alt="profile picture "></div>
-        <p><span class="whatsapp-name">Safer</span><br><small>Typically replies within few minutes</small></p>
 
-      </div>
-      <div class='get-new hide'>
-        <div id='get-label'></div>
-        <div id='get-nama'></div>
-      </div>
-    </div>
-    <div class='home-chat'>
-
-    </div>
-    <div class='start-chat'>
-      <div pattern="https://elfsight.com/assets/chats/patterns/whatsapp.png" class="WhatsappChat__Component-sc-1wqac52-0 whatsapp-chat-body">
-        <div class="WhatsappChat__MessageContainer-sc-1wqac52-1 dAbFpq">
-          <div style="opacity: 0;" class="WhatsappDots__Component-pks5bf-0 eJJEeC">
-            <div class="WhatsappDots__ComponentInner-pks5bf-1 hFENyl">
-              <div class="WhatsappDots__Dot-pks5bf-2 WhatsappDots__DotOne-pks5bf-3 ixsrax"></div>
-              <div class="WhatsappDots__Dot-pks5bf-2 WhatsappDots__DotTwo-pks5bf-4 dRvxoz"></div>
-              <div class="WhatsappDots__Dot-pks5bf-2 WhatsappDots__DotThree-pks5bf-5 kXBtNt"></div>
-            </div>
-          </div>
-          <div style="opacity: 1;" class="WhatsappChat__Message-sc-1wqac52-4 kAZgZq">
-            <div class="WhatsappChat__Author-sc-1wqac52-3 bMIBDo">Safer</div>
-            <div class="WhatsappChat__Text-sc-1wqac52-2 iSpIQi">Hi there 👋<br><br>How can I help you?</div>
-            <div class="WhatsappChat__Time-sc-1wqac52-5 cqCDVm"></div>
-          </div>
-        </div>
-      </div>
-
-      <div class='blanter-msg'>
-        <textarea id='chat-input' placeholder='Write a response' maxlength='120' row='1'></textarea>
-        <a href='javascript:void;' id='send-it'><svg viewBox="0 0 448 448"><path d="M.213 32L0 181.333 320 224 0 266.667.213 416 448 224z"/></svg></a>
-
-      </div>
-    </div>
-    <div id='get-number'></div><a class='close-chat' href='javascript:void'>×</a>
-  </div>
-  <a class='blantershow-chat' href='javascript:void' title='Show Chat'><svg width="20" viewBox="0 0 24 24"><defs/><path fill="#eceff1" d="M20.5 3.4A12.1 12.1 0 0012 0 12 12 0 001.7 17.8L0 24l6.3-1.7c2.8 1.5 5 1.4 5.8 1.5a12 12 0 008.4-20.3z"/><path fill="#4caf50" d="M12 21.8c-3.1 0-5.2-1.6-5.4-1.6l-3.7 1 1-3.7-.3-.4A9.9 9.9 0 012.1 12a10 10 0 0117-7 9.9 9.9 0 01-7 16.9z"/><path fill="#fafafa" d="M17.5 14.3c-.3 0-1.8-.8-2-.9-.7-.2-.5 0-1.7 1.3-.1.2-.3.2-.6.1s-1.3-.5-2.4-1.5a9 9 0 01-1.7-2c-.3-.6.4-.6 1-1.7l-.1-.5-1-2.2c-.2-.6-.4-.5-.6-.5-.6 0-1 0-1.4.3-1.6 1.8-1.2 3.6.2 5.6 2.7 3.5 4.2 4.2 6.8 5 .7.3 1.4.3 1.9.2.6 0 1.7-.7 2-1.4.3-.7.3-1.3.2-1.4-.1-.2-.3-.3-.6-.4z"/></svg> Chat with Us</a>
-<!--Start Social Icon-->
-<div class="fixed-icon">
-	<div class="f-icon f1"><a href="https://www.facebook.com/Safer4Free/" target="_blank" ><i class="fab fa-facebook-f"></i></a></div>
-	<div class="f-icon f2"><a href="http://twitter.com/safer4free" target="_blank"><i class="fab fa-twitter"></i></a></div>
-	<div class="f-icon f3"><a href="https://www.linkedin.com/in/safer-4free-107287132" target="_blank"><i class="fab fa-linkedin-in"></i></a></div>
-	<div class="f-icon f3"><a href="https://wa.me/?text={{ urlencode('https://safer.travel/') }}" target="_blank"><i class="fab fa-whatsapp"></i></a></div>
-	<div class="f-icon f4"><a href="https://www.instagram.com/safer4free.official/" target="_blank" ><i class="fab fa-instagram"></i></a></div>
-</div>
-<!--End Social Icon-->
 
   <!-- explore turkey -->
   <section class="investigtion">
@@ -430,55 +375,7 @@
 @section('adds_js')
 <script>
     $(document).ready(function() {
-// whts pp
-$(document).on("click", "#send-it", function() {
-  var a = document.getElementById("chat-input");
-  if ("" != a.value) {
-    var b = $("#get-number").text(),
-      c = document.getElementById("chat-input").value,
-      d = "https://web.whatsapp.com/send",
-      e = b,
-      f = "&text=" + c;
-    if (
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      )
-    )
-      var d = "whatsapp://send";
-    //   +31 6 29320129
-    var g = d + "?phone=+905445019185" + e + f;
-    window.open(g, "_blank");
-  }
-}),
-  $(document).on("click", ".informasi", function() {
-    (document.getElementById("get-number").innerHTML = $(this)
-      .children(".my-number")
-      .text()),
-      $(".start-chat,.get-new")
-        .addClass("show")
-        .removeClass("hide"),
-      $(".home-chat,.head-home")
-        .addClass("hide")
-        .removeClass("show"),
-      (document.getElementById("get-nama").innerHTML = $(this)
-        .children(".info-chat")
-        .children(".chat-nama")
-        .text()),
-      (document.getElementById("get-label").innerHTML = $(this)
-        .children(".info-chat")
-        .children(".chat-label")
-        .text());
-  }),
-  $(document).on("click", ".close-chat", function() {
-    $("#whatsapp-chat")
-      .addClass("hide")
-      .removeClass("show");
-  }),
-  $(document).on("click", ".blantershow-chat", function() {
-    $("#whatsapp-chat")
-      .addClass("show")
-      .removeClass("hide");
-  });
+
 
 $('.dynamic').change(function() {
 
