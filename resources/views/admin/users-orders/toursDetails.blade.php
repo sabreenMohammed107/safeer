799147c3618071a59fb9b0cab8cdba99d0641029
sabreenMohammed107 <sabreenm312@gmail.com>
2023-03-01 02:58:59 +0200

    update order admin

--- resources/views/admin/users-orders/toursDetails.blade.php
+++ resources/views/admin/users-orders/toursDetails.blade.php
@@ -23,14 +23,15 @@ data-kt-check-target="#kt_ecommerce_category_table .form-check-input"
 value="1" />
 </div>
 </th>
-<th class="min-w-200px">tour_name</th>
-<th class="min-w-100px">tour_type</th>
+<th class="min-w-70px">Tour ID</th>
+<th class="min-w-200px">Tour Name</th>
+<th class="min-w-100px">Tour Type</th>
 
-<th class="text-end min-w-70px">tour_cost</th>
-<th class="text-end min-w-100px">tour_date</th>
+<th class="text-end min-w-70px">Tour Cost</th>
+<th class="text-end min-w-100px">Tour Date</th>
 
-<th class="text-end min-w-100px">adults_count</th>
-<th class="text-end min-w-70px">children_count</th>
+<th class="text-end min-w-100px">Adults Count</th>
+<th class="text-end min-w-70px">children Count</th>
 <th class="text-end min-w-70px">line_cost</th>
 
 </tr>
@@ -49,6 +50,17 @@ value="1" />
 </div>
 </td>
 <!--end::Checkbox-->
+<td>
+    <div class="d-flex align-items-center">
+
+    <div class="ms-5">
+    <!--begin::Title-->
+
+   {{ $detail->tour_id}}</a>
+    <!--end::Title-->
+    </div>
+    </div>
+    </td>
 <!--begin::Category=-->
 <td>
 <div class="d-flex align-items-center">
@@ -102,6 +114,96 @@ data-kt-ecommerce-category-filter="category_name" >{{ $detail->tour_name?? ''}}<
 </div>
     </div>
     <!--end::General options-->
+      <!--begin::General options-->
+      <div class="card card-flush py-4">
+        <!--begin::Card header-->
+        <div class="card-header">
+            <div class="card-title">
+                <h2>Holder Details </h2>
+
+            </div>
+        </div>
+        <!--end::Card header-->
+         <!--begin::Table-->
+<div class="card-body pt-0">
+<table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_ecommerce_category_table">
+<!--begin::Table head-->
+<thead>
+<!--begin::Table row-->
+<tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
+<th class="w-10px pe-2">
+<div class="form-check form-check-sm form-check-custom form-check-solid me-3">
+<input class="form-check-input" type="checkbox" data-kt-check="true"
+data-kt-check-target="#kt_ecommerce_category_table .form-check-input"
+value="1" />
+</div>
+</th>
+
+<th class="min-w-200px">salutation</th>
+<th class="min-w-100px">holder name</th>
+<th class="text-end min-w-70px">mobile</th>
+<th class="text-end min-w-100px">notes</th>
+<th class="text-end min-w-70px">email </th>
+
+</tr>
+<!--end::Table row-->
+</thead>
+<!--end::Table head-->
+<!--begin::Table body-->
+<tbody class="fw-bold text-gray-600">
+{{-- @foreach ($persons as $index => $person) --}}
+<!--begin::Table row-->
+<tr>
+<!--begin::Checkbox-->
+<td>
+<div class="form-check form-check-sm form-check-custom form-check-solid">
+<input class="form-check-input" type="checkbox" value="1" />
+</div>
+</td>
+<!--end::Checkbox-->
+<!--begin::Category=-->
+<td>
+<div class="d-flex align-items-center">
+
+<div class="ms-5">
+<!--begin::Title-->
+
+<a href="#" class="text-gray-800 text-hover-primary fs-5 fw-bolder mb-1"
+data-kt-ecommerce-category-filter="category_name" >{{$order->holder_salutation ?? '' }}</a>
+<!--end::Title-->
+</div>
+</div>
+</td>
+
+<!--begin::Qty=-->
+<td class="text-strt pe-0" data-order="15">
+<span class="fw-bolder ms-3">{{$order->holder_name ?? '' }}</span>
+</td>
+<!--end::Qty=-->
+<td class="text-center pe-0" data-order="15">
+<span class="fw-bolder ms-3">{{ $order->holder_mobile ?? '' }}</span>
+</td>
+
+<td class="text-center pe-0" data-order="15">
+<span class="fw-bolder ms-3">{{ $order->notes ?? '' }}</span>
+</td>
+
+<td class="text-center pe-0" data-order="15">
+<span class="fw-bolder ms-3">{{ $order->holder_email ?? '' }}</span>
+</td>
+
+</tr>
+<!--end::Table row-->
+{{-- @endforeach --}}
+
+
+</tbody>
+<!--end::Table body-->
+</table>
+<!--end::Table-->
+</div>
+    </div>
+    <!--end::General options-->
                    <!--begin::General options-->
                    <div class="card card-flush py-4">
                     <!--begin::Card header-->
@@ -130,6 +232,8 @@ data-kt-ecommerce-category-filter="category_name" >{{ $detail->tour_name?? ''}}<
 <th class="min-w-200px">Person Name</th>
 <th class="min-w-100px">P/Type</th>
 <th class="text-end min-w-70px">Person Mobile</th>
+<th class="text-end min-w-100px">Age</th>
+<th class="text-end min-w-70px">Person Cost</th>
 
 </tr>
 <!--end::Table row-->
@@ -170,6 +274,13 @@ data-kt-ecommerce-category-filter="category_name" >{{ $detail->tour_name?? ''}}<
     <span class="fw-bolder ms-3">{{ $person->mobile ?? '' }}</span>
     </td>
 
+    <td class="text-center pe-0" data-order="15">
+        <span class="fw-bolder ms-3">{{ $person->age ?? '' }}</span>
+        </td>
+
+        <td class="text-center pe-0" data-order="15">
+            <span class="fw-bolder ms-3">{{ $person->person_cost ?? '' }}</span>
+            </td>
 
     </tr>
     <!--end::Table row-->
