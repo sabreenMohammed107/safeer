e060ac24f9f7b6f4525169a5fb711ad04d6b7797
sabreenMohammed107 <sabreenm312@gmail.com>
2023-04-05 19:35:03 +0200

    te

--- resources/views/components/website/header/home.blade.php
+++ resources/views/components/website/header/home.blade.php
@@ -2,7 +2,7 @@
 
 <div class="slider_section">
     <div class="slider_details">
-        <h1 style="font-size: 25px">
+        <h1 >
             @if (LaravelLocalization::getCurrentLocale() === 'en')
             {{ $company->master_page_entitle }}  <br> {{ $company->master_page_ensubtitle }}
 
