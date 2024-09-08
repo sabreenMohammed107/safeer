793e2ce684be7a699846b19d567ebcd12518e2d4
AhmdSydYsf <a.sayed.xc@gmail.com>
2024-09-08 11:51:03 +0400

    a

--- resources/views/layout/website/layout.blade.php
+++ resources/views/layout/website/layout.blade.php
@@ -86,7 +86,7 @@
 
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-    @livewireStyles()
+    @livewireStyles
     @yield("adds_css")
     <title> {{$title}} | Safer Book Your Dream Vacations</title>
 </head>
@@ -408,7 +408,7 @@
     {{-- <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script> --}}
     <script src="{{ asset('/website_assets/js/bootstrap-datepicker.min.js') }}"></script>
     <script src="{{ asset('/website_assets/js/date_picker.js') }}"></script>
-    @livewireScripts()
+    @livewireScripts
     <!-- Slick.s library -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script src="{{ asset('/website_assets/slick/slick-1.8.1/slick/slick.min.js') }}"></script>
