d0c22d258a44866c39444b89fec5faabf86469c1
sabreen mohammed <sabreen@wedomore.com>
2024-10-19 00:27:43 +0300

    change to old to work select2

--- resources/views/layout/footerscripts.blade.php
+++ resources/views/layout/footerscripts.blade.php
@@ -15,7 +15,7 @@
      <script src="{{asset('dist/assets/js/custom/apps/ecommerce/catalog/categories.js')}}"></script>
      <script src="{{asset('dist/assets/js/custom/apps/projects/users/users.js')}}"></script>
 
-     <script defer src="{{asset('dist/assets/plugins/custom/formrepeater/formrepeater.bundle.js')}}"></script>
+     <script src="{{asset('dist/assets/plugins/custom/formrepeater/formrepeater.bundle.js')}}"></script>
      <!--end::Page Vendors Javascript-->
 
 {{-- select 2 --}}
@@ -30,26 +30,26 @@
 {{-- eend of select 2 --}}
 		<!--end::Page Vendors Javascript-->
 		<!--begin::Page Custom Javascript(used by this page)-->
-		<script defer src="{{asset('dist/assets/js/custom/apps/ecommerce/catalog/save-product.js')}}"></script>
+		<script src="{{asset('dist/assets/js/custom/apps/ecommerce/catalog/save-product.js')}}"></script>
 
      <!--begin::Page Custom Javascript(used by this page)-->
-     <script defer src="{{asset('dist/assets/js/custom/apps/ecommerce/catalog/save-category.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/apps/ecommerce/catalog/save-category.js')}}"></script>
 <!--begin::Page Custom Javascript(used by this page)-->
 
-     <script defer src="{{asset('dist/assets/js/widgets.bundle.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/widgets.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/apps/chat/chat.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/upgrade-plan.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/create-project/type.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/create-project/budget.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/create-project/settings.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/create-project/team.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/create-project/targets.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/create-project/files.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/create-project/complete.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/create-project/main.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/users-search.js')}}"></script>
-     <script defer src="{{asset('dist/assets/js/custom/utilities/modals/new-target.js')}}"></script>
+     <script src="{{asset('dist/assets/js/widgets.bundle.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/widgets.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/apps/chat/chat.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/upgrade-plan.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/create-project/type.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/create-project/budget.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/create-project/settings.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/create-project/team.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/create-project/targets.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/create-project/files.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/create-project/complete.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/create-project/main.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/users-search.js')}}"></script>
+     <script src="{{asset('dist/assets/js/custom/utilities/modals/new-target.js')}}"></script>
 
 
      @yield('scripts')
--- resources/views/layout/style.blade.php
+++ resources/views/layout/style.blade.php
@@ -1,49 +1,49 @@
 
-		<link rel="preload" href="https://preview.keenthemes.com/craft" />
-		<link rel="shortcut icon" href="{{asset('dist/assets/media/logos/favicon.ico')}}" />
-		<!--begin::Fonts-->
-		<link rel="preload" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" />
-		<!--end::Fonts-->
-        <link  href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" rel="preload">
+<link rel="canonical" href="https://preview.keenthemes.com/craft" />
+<link rel="shortcut icon" href="{{asset('dist/assets/media/logos/favicon.ico')}}" />
+<!--begin::Fonts-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" />
+<!--end::Fonts-->
+<link href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" rel="Stylesheet">
 
-		<!--begin::Page Vendor Stylesheets(used by this page)-->
-		<link href="{{asset('dist/assets/plugins/custom/leaflet/leaflet.bundle.css')}}" rel="stylesheet" type="text/css" />
-		<link href="{{asset('dist/assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
-		<!--end::Page Vendor Stylesheets-->
-        	<!--begin::Page Vendor Stylesheets(used by this page)-->
-		<link href="{{asset('dist/assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
-		<!--end::Page Vendor Stylesheets-->
-		<!--begin::Global Stylesheets Bundle(used by all pages)-->
-        <link rel="preload"
-        href='https://bootstrap-tagsinput.github.io/bootstrap-tagsinput/dist/bootstrap-tagsinput.css'>
-		<link href="{{asset('dist/assets/plugins/global/plugins.bundle.css')}}" rel="preload" type="text/css" />
-		<link href="{{asset('dist/assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
-		<!--end::Global Stylesheets Bundle-->
-        {{-- select 2 --}}
-        {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
-        {{-- end select 2 --}}
+<!--begin::Page Vendor Stylesheets(used by this page)-->
+<link href="{{asset('dist/assets/plugins/custom/leaflet/leaflet.bundle.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('dist/assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
+<!--end::Page Vendor Stylesheets-->
+    <!--begin::Page Vendor Stylesheets(used by this page)-->
+<link href="{{asset('dist/assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
+<!--end::Page Vendor Stylesheets-->
+<!--begin::Global Stylesheets Bundle(used by all pages)-->
+<link rel='stylesheet'
+href='https://bootstrap-tagsinput.github.io/bootstrap-tagsinput/dist/bootstrap-tagsinput.css'>
+<link href="{{asset('dist/assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('dist/assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+<!--end::Global Stylesheets Bundle-->
+{{-- select 2 --}}
+{{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
+{{-- end select 2 --}}
 
 <!-- CSS -->
 
 
 <!-- Script -->
 
-        <style type="text/css">
-            .bootstrap-tagsinput .tag {
-               margin-right: 2px;
-               color: white !important;
-               background-color: #4137ce;
-               padding: .2em .6em .3em;
-               font-size: 100%;
-               font-weight: 700;
-               vertical-align: baseline;
-               border-radius: .25em;
-            }
-            .bootstrap-tagsinput{
-                width: 100%;
-            }
+<style type="text/css">
+    .bootstrap-tagsinput .tag {
+       margin-right: 2px;
+       color: white !important;
+       background-color: #4137ce;
+       padding: .2em .6em .3em;
+       font-size: 100%;
+       font-weight: 700;
+       vertical-align: baseline;
+       border-radius: .25em;
+    }
+    .bootstrap-tagsinput{
+        width: 100%;
+    }
 
-         </style>
+ </style>
 
 
 
