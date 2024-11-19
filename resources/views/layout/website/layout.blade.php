8fe0f0a1efd489384d97bff1273cbc5297ab81f2
sabreen mohammed <sabreen@wedomore.com>
2024-11-19 18:06:57 +0200

    add google script

--- resources/views/layout/website/layout.blade.php
+++ resources/views/layout/website/layout.blade.php
@@ -167,7 +167,13 @@
             display: none !important;
         }
     </style> --}}
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KWV4FRFJ');</script>
+    <!-- End Google Tag Manager -->
     @yield("adds_css")
     <title> {{$title}} | Safer Book Your Dream Vacations</title>
 </head>
