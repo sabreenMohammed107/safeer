8389f3203e6f954632c5b63460daeb06102eec94
sabreen mohammed <sabreen@wedomore.com>
2024-11-23 16:07:38 +0200

    add form controll

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -38,17 +38,17 @@
 
                 <div>
                     <label for="email">Email</label>
-                    <input id="email" type="email" name="email" value="{{ old('email', $email ?? '') }}" required>
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email', $email ?? '') }}" required>
                 </div>
 
                 <div>
                     <label for="password">New Password</label>
-                    <input id="password" type="password" name="password" required>
+                    <input id="password" type="password" class="form-control" name="password" required>
                 </div>
 
                 <div>
                     <label for="password_confirmation">Confirm Password</label>
-                    <input id="password_confirmation" type="password" name="password_confirmation" required>
+                    <input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required>
                 </div>
 
                 <button type="submit">Reset Password</button>
