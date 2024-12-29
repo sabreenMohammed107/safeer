78ed9e614d8b9b2599bc695f922bb46ea508cfb9
ahmdsydysf <a.sayed.xc@gmail.com>
2024-12-29 11:21:51 +0400

    finish ui for aplicant

--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,7 @@ use Illuminate\Support\Facades\Lang as Lang;
 Route::get('/applicant', function () {
     return view('website.applicantForm');
 });
+
 Route::get('/clear-cache', function () {
     Artisan::call('cache:clear');
     Artisan::call('route:clear');
